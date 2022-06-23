7d07d00984d883f5bc11e367368512dec0697828
RTa-technology <scpfoundationidcardmaker@gmail.com>
2022-06-23 18:49:05 +0900

    fix: _404

--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -1,33 +1,19 @@
-<script setup lang="ts">
-console.log("not found")
-</script>
 <template>
   <div id="content-wrap">
     <Side />
     <div id="main-content">
-      <div id="page-title"></div>
-      <div id="page-content"></div>
-      <div class="page-tags">
-        <span> </span>
-      </div>
-      <div id="page-info-break">
-        <div id="page-styles"></div>
-      </div>
-      <div id="page-options-container">
-        <div id="page-options-bottom" class="page-options-bottom">
-          <details class="btn btn-default" id="view-source-button">
-            <summary>Page Source</summary>
-            <div id="action-area">
-              <h1>Page source</h1>
-              <div class="page-source">
-                <pre class="page-source-code"></pre>
-              </div>
-            </div>
-          </details>
-        </div>
-
-        <div id="page-options-area-bottom"></div>
-      </div>
+      <div id="page-content">
+        <div style="max-width: 520px; margin: 0 auto;">
+          <div style="background-color: #600; border: solid 1px #600; border-radius: 20px; color: #fff; max-width: 700px; margin-bottom: 1.25rem; font-size: 150%; text-align: center; box-shadow:0 2px 6px rgb(0 0 0 / 50%), inset 0 10px 20px rgb(255 204 204 / 30%), inset 0 -15px 30px rgb(48 0 0 / 50%); line-height: 100%; padding: 0 10px; text-align: center; color: white; font-weight: bold;">
+          <p><span style="font-size:125%;">This page doesn't exist.</span></p>
+          </div>
+          <p>This page may have been moved or deleted, or it may have never existed. Pages can be deleted for a number of reasons: They may be deleted by their own authors, or they may reach the deletion threshold, as explained in the <a href="javascript:;">Deletions Guide</a>.</p>
+          <p>You may also look through the current deletions threads (stickied in <a href="javascript:;">Page Announcements</a>) if you are looking for a particular page that has been deleted.</p>
+          <p>If you want to create a new page, please first read the <a href="javascript:;">How to Contribute</a> guide.</p>
+          <hr>
+          <p style="text-align: center;">If you are ready to proceed, then you may <strong><a class="wiki-standalone-button" href="javascript:;">create the page</a></strong>.</p>
+          </div>
+      </div>      
     </div>
   </div>
 </template>
